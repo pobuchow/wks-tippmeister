c5e6a4c6ffc35979b7d848124bae802af3e79071
pobuchow <pobu1@hotmail.com>
2020-12-20 21:55:25 +0100

    added unique key for react elements - Scoreboard

--- src/app/components/Scoreboard.jsx
+++ src/app/components/Scoreboard.jsx
@@ -6,7 +6,7 @@ export const Scoreboard = ({ scores }) => (
     <div>
         <h3>scoreboard</h3>
         {_.orderBy(scores, ['points'], ['desc']).map(score =>(
-            <div>
+            <div key={score.id}>
                 {score.name} {score.points}
             </div>
         ))}
