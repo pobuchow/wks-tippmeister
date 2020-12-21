f42cf0012d678ccfdb8a793b7f19dc409efa7b29
pobuchow <pobu1@hotmail.com>
2020-12-21 22:21:54 +0100

    added basic routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
+import { Router, Route } from "react-router-dom";
+import { history } from "../store/history";
 
 export const Main = () => (
-  <Provider store={store}>
-    <div>
-        <ConnectedDashboard gameId={1}/>
-    </div>
-  </Provider>
+  <Router history={history}>
+    <Provider store={store}>
+      <div>
+        <Route
+          exact
+          path="/dashboard"
+          render={() => <ConnectedDashboard gameId={1} />}
+        />
+      </div>
+    </Provider>
+  </Router>
 );
