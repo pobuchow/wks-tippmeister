d8e3acedd8c66935bc35bc78eeb07f148b82884f
pobuchow <pobu1@hotmail.com>
2020-12-22 18:51:29 +0100

    fixed routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { ConnectedDashboard } from "./Dashboard";
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import { history } from "../store/history";
-import { ConnectedNavigation } from './Navigation';
+import { ConnectedNavigation } from "./Navigation";
 
-export const Main = () => (
-  <Router history={history}>
-    <Provider store={store}>
-      <div>
-        <ConnectedNavigation />
-        <Route
-          exact
-          path="/dashboard"
-          render={() => <ConnectedDashboard gameId={1} />}
-        />
-      </div>
-    </Provider>
-  </Router>
-);
+export const Main = () => {
+  const [gameId, setGameId] = useState(1);
+  return (
+    <BrowserRouter history={history}>
+      <Provider store={store}>
+        <div>
+          <ConnectedNavigation />
+          <Route
+            exact
+            path="/dashboard"
+            render={() => <ConnectedDashboard gameId={gameId} />}
+          />
+        </div>
+      </Provider>
+    </BrowserRouter>
+  );
+};
