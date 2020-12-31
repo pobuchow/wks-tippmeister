6058a557251a58eab9fc14c2c958bf9aafed13af
pobuchow <pobu1@hotmail.com>
2020-12-31 09:57:38 +0100

    init express server

---
+++ src/server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { connectDB } from './connectDB';
+
+let port = 8080;
+let app = express();
+
+app.listen(port, console.log('Server is running on port', port));
+
+/* app.get('/', (request, response)=>{
+    response.send('hello express');
+}); */
+
+app.use(
+    cors(),
+    bodyParser.urlencoded({extended: true}),
+    bodyParser.json()
+);
+
+export const addNewBet = async bet => {
+    let db = await connectDB();
+    let collection = db.collection(`bets`);
+    await collection.insertOne(bet);
+};
+
+app.post('/bets', async (request, response) => {
+    let bet = request.body.bet;
+    await addNewBet(bet);
+    response.status(200).send();
+});
---
+++ src/server/server.spec.js
@@ -0,0 +1,10 @@
+import { addNewBet } from "./server";
+
+addNewBet({
+  id: 11,
+  match: 3,
+  owner: 1,
+  goalsHomeTeam: 0,
+  goalsAwayTeam: 0,
+  game: 1,
+});
