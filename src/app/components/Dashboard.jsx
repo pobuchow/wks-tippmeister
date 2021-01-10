2fb42d9ed41503005607a9217d64d1ddee1b0eb0
pobuchow <pobu1@hotmail.com>
2021-01-10 12:26:38 +0100

    render AddGameUserForm only for game hosts users

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { ConnectedNextMatch } from './NextMatch';
-import { ConnectedLastMatch } from './LastMatch';
-import { ConnectedScoreboard } from './Scoreboard';
-import { ConnectedAddGameUserForm } from './AddGameUserForm';
+import { ConnectedNextMatch } from "./NextMatch";
+import { ConnectedLastMatch } from "./LastMatch";
+import { ConnectedScoreboard } from "./Scoreboard";
+import { ConnectedAddGameUserForm } from "./AddGameUserForm";
 
-export const Dashboard = ({ game }) => (
+export const Dashboard = ({ userId, game }) => (
   <div>
     <h2>dashboard</h2>
-    <ConnectedAddGameUserForm game={game} />
+    {_.includes(game.hosts, userId) && <ConnectedAddGameUserForm game={game} />}
     <ConnectedLastMatch game={game.id} />
     <ConnectedNextMatch game={game.id} />
-    <ConnectedScoreboard 
-      users={game.users} 
-      scores={game.scores} 
-      game={game.id} />
+    <ConnectedScoreboard
+      users={game.users}
+      scores={game.scores}
+      game={game.id}
+    />
   </div>
 );
 
@@ -23,7 +24,8 @@ function mapState2Props(state, ownProps) {
   const gameId = ownProps.match.params.id;
   const game = _.find(state.games, ["id", gameId]);
   return {
-    game: game
+    userId: state.session.id,
+    game: game,
   };
 }
 
