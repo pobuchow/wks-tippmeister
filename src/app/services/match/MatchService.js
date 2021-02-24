b6dc6a434113a0ec81ee25e2e28a2d1ccd37f9dc
pobuchow <pobu1@hotmail.com>
2021-02-24 20:26:54 +0100

    match service jest test

--- src/app/services/match/MatchService.js
+++ src/app/services/match/MatchService.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const goalMinValue = 0;
 const goalMaxValue = 9;
 
--- src/app/services/match/MatchService.test.js
+++ src/app/services/match/MatchService.test.js
@@ -3,6 +3,38 @@ import { matchService } from './MatchService';
 describe(`Match Service`, () => {
     let dateSpy;
 
+    let listOfMatches = [
+        {
+            id: '1',
+            event_datetime: new Date(2020, 12-1, 3, 18, 0, 0, 0),
+            homeTeam: 'Śląsk Wrocław',
+            awayTeam: 'Warta Poznań',
+            goalsHomeTeam: 2,
+            goalsAwayTeam: 1
+        },{
+            id: '2',
+            event_datetime: new Date(2020, 12-1, 11, 18, 0, 0, 0),
+            homeTeam: 'Zagłębie Lubin',
+            awayTeam: 'Śląsk Wrocław',
+            goalsHomeTeam: 2,
+            goalsAwayTeam: 1
+        },{
+            id: '3',
+            event_datetime: new Date(2021, 2-1, 1, 18, 0, 0, 0),
+            homeTeam: 'Stal Mielec',
+            awayTeam: 'Śląsk Wrocław',
+            goalsHomeTeam: null,
+            goalsAwayTeam: null
+        },{
+            id: '4',
+            event_datetime: new Date(2021, 2-1, 6, 18, 0, 0, 0),
+            homeTeam: 'Piast Gliwice',
+            awayTeam: 'Śląsk Wrocław',
+            goalsHomeTeam: null,
+            goalsAwayTeam: null
+        }
+    ];
+
     beforeAll(() => {
         dateSpy = jest
             .spyOn(global.Date, 'now')
@@ -48,4 +80,45 @@ describe(`Match Service`, () => {
         }
         expect(matchService.isMatchOver(match)).toEqual(false);
     });
+
+    it(`should execute function if goals pass min-max criterion`, () => {
+        const setter = jest.fn();
+        matchService.handleGoalInput('5', setter);
+        expect(setter).toHaveBeenCalledWith(5);
+    });
+
+    it(`should not execute function if goals not pass max criterion`, () => {
+        const setter = jest.fn();
+        matchService.handleGoalInput('10', setter);
+        expect(setter).not.toHaveBeenCalled();
+    });
+
+    it(`should not execute function if goals not pass min criterion`, () => {
+        const setter = jest.fn();
+        matchService.handleGoalInput('-1', setter);
+        expect(setter).not.toHaveBeenCalled();
+    });
+
+    it(`should return last match from list of matches`, () => {
+        expect(matchService.getLastMatch(listOfMatches)).toEqual({
+            id: '2',
+            event_datetime: new Date(2020, 12-1, 11, 18, 0, 0, 0),
+            homeTeam: 'Zagłębie Lubin',
+            awayTeam: 'Śląsk Wrocław',
+            goalsHomeTeam: 2,
+            goalsAwayTeam: 1
+        });
+    });
+
+    it(`should return next match from list of matches`, () => {
+        expect(matchService.getNextMatch(listOfMatches)).toEqual({
+            id: '3',
+            event_datetime: new Date(2021, 2-1, 1, 18, 0, 0, 0),
+            homeTeam: 'Stal Mielec',
+            awayTeam: 'Śląsk Wrocław',
+            goalsHomeTeam: null,
+            goalsAwayTeam: null
+        });
+    });
+    
 });
