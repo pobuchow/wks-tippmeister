81d69b8968d36daff4dd824272f300ff57b149cc
pobuchow <pobu1@hotmail.com>
2021-03-02 21:54:56 +0100

    refactored docker scripts

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,3 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+ 
+set -e
+ 
+BASE_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+PROJECT_PATH="$(dirname "$BASE_PATH")"
 
-docker build ./../ -t wks-tippmeister:dev -f ./../Dockerfile
+echo ==========================================================================
+echo Building Docker images...
+echo ==========================================================================
+docker-compose -f "$PROJECT_PATH/docker-compose.yml" build
--- scripts/stop-docker.sh
+++ scripts/stop-docker.sh
@@ -4,8 +4,8 @@ set -e
  
 BASE_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 PROJECT_PATH="$(dirname "$BASE_PATH")"
-
+ 
 echo ==========================================================================
-echo Starting Docker containers...
+echo Stopping Docker containers...
 echo ==========================================================================
-docker-compose -f "$PROJECT_PATH/docker-compose.yml" up -d
+docker-compose -f "$PROJECT_PATH/docker-compose.yml" down
